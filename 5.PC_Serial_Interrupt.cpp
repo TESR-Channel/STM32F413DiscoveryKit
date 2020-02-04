d77dcc517b00257320369a21dcae7e1bcad6e6fc
TESR <ceo.anoney.potter@gmail.com>
2020-02-05 02:25:35 +0700

    Add files via upload

---
+++ 5.PC_Serial_Interrupt.cpp
@@ -0,0 +1,36 @@
+#include "mbed.h"
+
+DigitalOut led1(LED1);
+
+/* No need Serial to UART just usb debugger. */
+Serial UART(SERIAL_TX, SERIAL_RX);
+
+/* External interrupt Handeller */
+void UARTIrq();
+
+int main()
+{
+    UART.baud(115200);
+    UART.attach(UARTIrq,Serial::RxIrq);
+    printf("Start\r\nTurn ON press '1' Turn OFF press '0'\r\n");
+    while(1) 
+    {
+        thread_sleep_for(10);
+    }
+}
+
+void UARTIrq()
+{
+    char input_char = UART.getc();
+    printf("%c \n\r",input_char);
+    if(input_char == '1')
+    {
+        printf("Press '1' turn on \r\n");
+        led1.write(1);
+    }
+    else if(input_char == '0')
+    {
+        printf("Press '0' turn off \r\n");
+        led1.write(0);
+    }
+}
