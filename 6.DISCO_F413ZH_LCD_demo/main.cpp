ab3e8f6c55e68bf9dd743e274c3c6933e2bb4cc6
TESR <ceo.anoney.potter@gmail.com>
2020-02-06 12:10:21 +0700

    Add files via upload

---
+++ 6.DISCO_F413ZH_LCD_demo/main.cpp
@@ -0,0 +1,20 @@
+#include "mbed.h"
+#include "BSP_DISCO_F413ZH\\Drivers\\BSP\\STM32F413H-Discovery\\stm32f413h_discovery_lcd.h"
+
+int main()
+{
+    BSP_LCD_Init();
+
+    /* Clear the LCD */
+    BSP_LCD_Clear(LCD_COLOR_WHITE);
+
+    BSP_LCD_DisplayStringAt(0, LINE(1), (uint8_t *)"MBED EXAMPLE", CENTER_MODE);
+    thread_sleep_for(2);
+    BSP_LCD_Clear(LCD_COLOR_GREEN);
+    BSP_LCD_SetBackColor(LCD_COLOR_GREEN);
+    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+    BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"DISCOVERY STM32F413ZH", CENTER_MODE);
+
+    while(1) {
+    }
+}
