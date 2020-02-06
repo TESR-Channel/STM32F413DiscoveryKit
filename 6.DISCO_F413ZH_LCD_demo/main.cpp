1ccbeb87729f462c7df688a7b5e0e93c59bbb62c
TESR <ceo.anoney.potter@gmail.com>
2020-02-06 12:11:04 +0700

    Update main.cpp

--- 6.DISCO_F413ZH_LCD_demo/main.cpp
+++ 6.DISCO_F413ZH_LCD_demo/main.cpp
@@ -1,5 +1,5 @@
 #include "mbed.h"
-#include "BSP_DISCO_F413ZH\\Drivers\\BSP\\STM32F413H-Discovery\\stm32f413h_discovery_lcd.h"
+#include "Lib_DISCO_F413ZH\\Drivers\\BSP\\STM32F413H-Discovery\\stm32f413h_discovery_lcd.h"
 
 int main()
 {
